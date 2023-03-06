5d6fbbb41682549a8aff8b28f4e0959d931a5c47
DanyElAlgo <64906201+DanyElAlgo@users.noreply.github.com>
2023-03-06 09:35:58 -0400

    ejercicio 1
    añadí la solucion del ejercicio 1.

---
+++ Ejercicio_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double numero = 6;
+    double numero_2 = 2.75;
+    
+    cout<< static_cast <int> (numero + numero_2);
+
+    return 0;
+}
