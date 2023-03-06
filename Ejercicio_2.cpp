65899d950310a11c343931e958cfc11b757495a3
DanyElAlgo <64906201+DanyElAlgo@users.noreply.github.com>
2023-03-06 09:37:53 -0400

    ejercicio 2
    solucion al ejercicio 2

---
+++ Ejercicio_2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int
+main ()
+{
+  double number_1 = 5;
+  double number_2 = 2;
+  cout << number_1 / number_2 << endl;
+
+  number_1 = 5;
+  number_2 = 0;
+  cout << number_1 / number_2 << endl;
+
+  return 0;
+
+}
