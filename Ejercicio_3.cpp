1f1ecf284f073a8b3b4750dd9c5ebc68a5653a14
DanyElAlgo <64906201+DanyElAlgo@users.noreply.github.com>
2023-03-06 09:38:20 -0400

    ejercicio 3
    solucion al ejercicio 3.

---
+++ Ejercicio_3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int
+main ()
+{
+
+  int width;
+  int height;
+  
+  cout << "Enter rectangle width and height" << endl;
+  cin >> width >> height;
+  cout << "Rectangle area is " << width * height << endl;
+}
