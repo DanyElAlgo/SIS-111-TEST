cbc309d351a9825afc60d37560110f379dc5344e
DanyElAlgo <64906201+DanyElAlgo@users.noreply.github.com>
2023-03-14 17:24:18 -0400

    Create ejercicio_8_sol.cpp

---
+++ practico_3/ejercicio_8_sol.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+ string query1;
+ string query2;
+ 
+ cin >> query1 >> query2;
+ 
+ if (query1 == "green") {
+ 	cout << "[green]" << endl;}
+ if (query2 == "parrot") {
+ 	cout << "[parrot]" << endl;}
+}
+//Este ejercicio tampoco pude entenderlo
