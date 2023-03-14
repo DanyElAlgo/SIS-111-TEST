c291b26b0c3336e806c6c55e06e2579c0a2f4747
DanyElAlgo <64906201+DanyElAlgo@users.noreply.github.com>
2023-03-14 17:22:39 -0400

    Create ejercicio_7_sol.cpp

---
+++ practico_3/ejercicio_7_sol.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+ string query1;
+ string query2;
+ 
+ cin >> query1 >> query2;
+ 
+ if (query1 == "green") {
+ 	cout << "5" << endl;}
+ if (query2 == "parrot") {
+ 	cout << "12" << endl;}
+}
+//No he entendido nada de nada sobre este ejercicio...
