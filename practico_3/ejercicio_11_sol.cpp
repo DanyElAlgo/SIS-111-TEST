a9e34d194c1a0bbd73bbc624429e96f7417a5c80
DanyElAlgo <64906201+DanyElAlgo@users.noreply.github.com>
2023-03-23 16:38:50 -0400

    Ejercicio 11 solucionado :D

---
+++ practico_3/ejercicio_11_sol.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int base;
+    int exp;
+    
+    cin >> base >> exp;
+    
+    cout << pow(base, exp);
+    
+}
+
+//he tenido que hacer una investigación sobre cómo hacer potencias en lenguaje C++, y requería una nueva librería y un nuevo comando para ello
