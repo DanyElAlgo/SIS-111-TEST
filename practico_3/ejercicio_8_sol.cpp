922b3fc4437c86c8200aaef3a57f73f06323cff7
DanyElAlgo <64906201+DanyElAlgo@users.noreply.github.com>
2023-03-30 15:05:16 -0400

    Update ejercicio_8_sol.cpp

--- practico_3/ejercicio_8_sol.cpp
+++ practico_3/ejercicio_8_sol.cpp
@@ -6,7 +6,7 @@ int main() {
  getline (cin, query);
 
  cout << "[";
-for (int i = 0; i <= query.size(); i++) { cout << query[i];
+for (int i = 0; i < query.size(); i++) { cout << query[i];
      if (query[i+1] == ' ' ) {
         cout << "]\n["; i += 1;
      }
