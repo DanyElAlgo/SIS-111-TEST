b020277c8ea235c122553e855bdbf4efe5ae04c7
DanyElAlgo <64906201+DanyElAlgo@users.noreply.github.com>
2023-03-31 09:36:57 -0400

    Create ejercicio_4_sol.cpp

---
+++ practico_4/ejercicio_4_sol.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin >> a;
+
+    switch(a){
+        case 1:
+        cout << "Hoy es lunes...";
+        break;
+        case 2:
+        cout << "Hoy es martes.";
+        break;
+        case 3:
+        cout << "Hoy es miércoles.";
+        break;
+        case 4:
+        cout << "Hoy es jueves.";
+        break;
+        case 5:
+        cout << "Hoy es viernes!";
+        break;
+        case 6:
+        cout << "Hoy es sábado!";
+        break;
+        case 7:
+        cout << "Hoy es domingo!";
+        break;
+
+    }
+
+}
